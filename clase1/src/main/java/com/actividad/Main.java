30bee2d0f414b0baf3a825e5eea4bedb3bcd7cff
SrAndres6 <andresrjramillo671@gmail.com>
2025-10-23 15:04:22 -0500

    creacion del proyecto, la declaracion de variables y los primeros identificadores

---
+++ clase1/src/main/java/com/actividad/Main.java
@@ -0,0 +1,16 @@
+package com.actividad;
+
+public class Main {
+    public static void main(String[] args) {
+        //declaracion de variables de tipo int y double
+        int numEntero = 10;
+        System.out.println("el numero es:" + numEntero);
+        double numDecimal = 5.5;
+        System.out.println("el numero decimal es:" + numDecimal);
+
+        //declaracion de variables siguiendo el lowerCamelCase
+        String nombreCompleto = "Andres Garcia";
+        System.out.println("el nombre es:" + nombreCompleto);
+
+    }
+}
